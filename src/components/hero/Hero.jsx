47d501392b7819482aaddeb65bac4d35ccca0c6c
AramirRp <antoinerp@gmail.com>
2024-05-16 15:04:38 +0200

    avancée css

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,33 +1,36 @@
 import AnchorLink from "react-anchor-link-smooth-scroll";
 
-
 import "./hero.css";
 import profile_img from "../../assets/profile_img.jpg";
-import cv from "../../assets/cv-2024.jpg"
-import cv_pdf from "../../assets/cv-2024.pdf"
+import cv from "../../assets/cv-2024.jpg";
+import cv_pdf from "../../assets/cv-2024.pdf";
 
 export const Hero = () => {
-
-
   return (
-    <div id="home" className="hero">
-      <img src={profile_img} alt="" className="profileImg"/>
-      <h2>
-        <span>Hello Im Antoine ! </span> Newbie développeur frontend/fullstack sur Paris.{" "}
+    <div id="home" className="hero flex items-center flex-col gap-3 md:gap-5">
+      <img
+        src={profile_img}
+        alt=""
+        className="profileImg h-1/5 w-[34%] mt-[82px] rounded-[350px] md:mt-[183px] lg:mt-[149px]"
+      />
+      <h2 className="text-center w-[81%] text-[17px] font-semibold">
+        <span className="color-sent bg-clip-text">Hello I'm Antoine ! </span> Newbie développeur frontend/fullstack
+        sur Paris.{" "}
       </h2>
-      <p>Ceci est un portfolio, il parait.</p>
-      <div className="hero-action">
+      <p className="text-center text-xs leading-[30px]">Ceci est un portfolio, il parait.</p>
+      <div className="hero-action flex items-center gap-5 text-xl font-medium mb-10">
         <div className="hero-connect">
           <AnchorLink className="anchor-link" offset={50} href="#contact">
             Envoyer une missive
           </AnchorLink>
         </div>
-        
-        <div className="hero-resume"> 
 
-        <button> Mon CV (il parait)</button>
+        <div className="hero-resume">
+          <button> Mon CV (il parait)</button>
 
-              <a href={cv_pdf} target="_blank"><img src={cv} alt="CV" className="CV" />  </a>          
+          <a href={cv_pdf} target="_blank">
+            <img src={cv} alt="CV" className="CV" />{" "}
+          </a>
         </div>
       </div>
     </div>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
 import { Turn as Hamburger } from "hamburger-react";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 
 import "./navbar.css";
 import logo from "../../assets/logo.svg";
 
 export const Navbar = () => {
   const Links = [
-    { name: "Accueil", link: "/" },
     { name: "A propos", link: "#about" },
     { name: "Compétences", link: "#services" },
     { name: "Portfolio", link: "#work" },
@@ -16,37 +16,44 @@ export const Navbar = () => {
   let [open, setOpen] = useState(false);
 
   return (
-    <div className='shadow-md fixed w-full border-gray-200 bg-gray-50'>
-    <div className='max-w-screen-xl md:flex-col lg:flex-row lg:flex-nowrap lg:justify-center flex flex-wrap items-center justify-between mx-auto p-4'>
-          <img className="logo md:h-8 md:w-[16%] " src={logo} alt="logo" />
+    <div className="shadow-md fixed w-full border-gray-200 bg-gray-50">
+      <div className="max-w-screen-xl md:flex-col lg:flex-row lg:flex-nowrap lg:justify-center flex flex-wrap items-center justify-between mx-auto p-4">
+        <img className="logo md:h-8 md:w-[16%] " src={logo} alt="logo" />
 
-      <div
-        onClick={() => setOpen(!open)}
-        className="inline-flex items-center justify-center p-2 w-10 h-10 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200  cursor-pointer md:hidden "
-      >
-        <Hamburger toggled={open} toggle={setOpen} />
-      </div>
+        <div
+          onClick={() => setOpen(!open)}
+          className="inline-flex items-center justify-center p-2 w-10 h-10 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200  cursor-pointer md:hidden "
+        >
+          <Hamburger toggled={open} toggle={setOpen} />
+        </div>
 
-      <ul
-        className={`left-0 absolute  pb-4 md:static w-full pl-4 transition-all duration-500 ease-in md:flex md:items-center md:flex-row md:justify-end md:gap-1 lg:gap-3 lg:mt-2 ${
-          open ? "top-20 bg-white opacity-95 md:bg-transparent " : "top-[-490px]"
-        }`}
-      >
-        {Links.map((link) => (
-          <li key={link.name} className="md:ml-5 md:content-end md:text-lg  text-xl my-7 rounded-lg flex flex-col cursor-pointer">
-            <a
-              href={link.link}
-              className="text-gray-800 hover:text-gray-400 duration-500"
+        <ul
+          className={`left-0 absolute  pb-4 md:static w-full pl-4 transition-all duration-500 ease-in md:flex md:items-center md:flex-row md:justify-end md:gap-1 lg:gap-3 lg:mt-2 ${
+            open
+              ? "top-20 bg-white opacity-95 md:bg-transparent "
+              : "top-[-490px]"
+          }`}
+        >
+          {Links.map((link) => (
+            <li
+              key={link.name}
+              className="md:ml-5 md:content-end md:text-lg  text-xl my-7 rounded-lg flex flex-col cursor-pointer"
             >
-              {link.name}
-            </a>
-          </li>
-        ))}
-        <button className="md:p-3 md:m-3 nav-connect text-[22px] cursor-pointer px-6 py-4 rounded-[50px]">
-          Missive virtuelle
-        </button>
-      </ul>
-    </div>
+              <AnchorLink className="anchor-link" offset={50} href={link.link}>
+                <a
+                  href={link.link}
+                  className="text-gray-800 hover:text-gray-400 duration-500"
+                >
+                  {link.name}
+                </a>
+              </AnchorLink>
+            </li>
+          ))}
+          <button className="md:p-3 md:m-3 nav-connect text-[22px] cursor-pointer px-6 py-4 rounded-[50px]">
+            <a href="#contact"> Missive virtuelle</a>
+          </button>
+        </ul>
+      </div>
     </div>
   );
 };
